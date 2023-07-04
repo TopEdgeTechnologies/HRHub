d830869d15eeeddc9235e54738ec53abc87ae4bc
ATHAR <dynamixsolsoft@gmail.com>
2023-07-04 14:47:57 +0500

    received changes

---
+++ HRHUBAPI/Models/UserLoginHistory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HRHUBAPI.Models;
+
+public partial class UserLoginHistory
+{
+    public int UserLoginHistoryId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public DateTime? SessionFrom { get; set; }
+
+    public DateTime? SessionTo { get; set; }
+
+    public int? CreateBy { get; set; }
+
+    public DateTime? CreatedOn { get; set; }
+
+    public int? UpdatedBy { get; set; }
+
+    public DateTime? UpdatedOn { get; set; }
+}
